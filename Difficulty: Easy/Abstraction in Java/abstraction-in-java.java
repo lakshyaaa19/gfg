0a3f462f87c65ec1c55d4885a50159e97f40ca65
lakshyaaa19 <lakshyaschourasia23@gmail.com>
2026-08-05 23:52:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Abstraction in Java/abstraction-in-java.java
@@ -0,0 +1,33 @@
+
+abstract class Shape {
+    String color;
+
+    
+    Shape(String c) {
+        color = c;
+    }
+
+    
+    String getColor() {
+        return color;
+    }
+
+    
+    abstract double getArea();
+}
+
+
+class Square extends Shape {
+    double side;
+
+    
+    Square(String c, double side) {
+        super(c); 
+        this.side = side;
+    }
+
+    
+    double getArea() {
+        return side * side;
+    }
+}
